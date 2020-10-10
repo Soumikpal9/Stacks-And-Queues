3d5a0cd75c378ca65c08d00150ad138b0a947a50
Soumikpal9 <soumikpal1997@gmail.com>
2020-10-10 14:39:38 +0530

    [SP]Added: Simple Stack Implemented By Linked List

--- src/com/cg/Executor.java
+++ src/com/cg/Executor.java
@@ -3,26 +3,19 @@ package com.cg;
 public class Executor {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Stack and Queue Problem!!!");
-		
+    
 		Node<Integer> firstNode = new Node<>(70);
     	Node<Integer> secondNode = new Node<>(30);
     	Node<Integer> thirdNode = new Node<>(56);
     	Node<Integer> fourthNode = new Node<>(66);
-    	Node<Integer> midNode = new Node<>(42);
     	
-    	LinkedList linkedList = new LinkedList();
-    	linkedList.add(firstNode);
-    	linkedList.add(secondNode);
-    	linkedList.add(thirdNode);
-    	linkedList.add(fourthNode);
+    	Stack stack = new Stack();
+    	stack.push(firstNode);
+    	stack.push(secondNode);
+    	stack.push(thirdNode);
+    	stack.push(fourthNode);
     	
-    	linkedList.printLinkedList();
-    	
-    	linkedList.addMiddle(midNode);
-    	linkedList.printLinkedList();
-    	
-    	linkedList.sortList();
-    	linkedList.printLinkedList();
-    	
+    	System.out.println("Here is the simple stack implemented by linked list : ");
+    	stack.printStack();
 	}
 }
--- src/com/cg/Stack.java
+++ src/com/cg/Stack.java
@@ -1,5 +1,17 @@
 package com.cg;
 
 public class Stack {
-
+	private final LinkedList linkedList;
+	
+	public Stack() {
+		this.linkedList = new LinkedList(); 
+	}
+	
+	public void push(INode newNode) {
+		linkedList.add(newNode);
+	}
+	
+	public void printStack() {
+		linkedList.printLinkedList();
+	}
 }
